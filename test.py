9c53c6cde245a3ef9e20aa5f81171150a74befe9
zasexton <zsexton@stanford.edu>
2022-07-20 00:10:52 -0700

    perserving simple test file for building a single tree in a cube volume

--- test.py
+++ test.py
@@ -4,11 +4,15 @@ import pyvista as pv
 cube = pv.Cube().triangulate().subdivide(3)
 
 s = svcco.surface()
-s.set_data(10*cube.points,normals=cube.point_normals)
+s.set_data(cube.points,normals=cube.point_normals)
 s.solve()
 s.build()
 
+# Warm up
 t = svcco.tree()
+t.convex = True
 t.set_boundary(s)
 t.set_root()
-t.n_add(10)
+t.n_add(200)
+
+t.export(gui=False)
